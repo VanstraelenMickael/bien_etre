3d2e48c548d2a0967ef340e64e5fbec61c0cdf4f
Ika <atomik.mlv@gmail.com>
2021-12-16 14:20:31 +0100

    ajout page détails service via ID

---
+++ src/Controller/ServicesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\CategorieDeServices;
+use App\Entity\Prestataire;
+use App\Entity\Images;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ServicesController extends AbstractController
+{
+    /**
+     * @Route("/services/{id}", name="services")
+     */
+    public function index(CategorieDeServices $service, EntityManagerInterface $entitymanager): Response
+    {
+        $repository = $entitymanager->getRepository(CategorieDeServices::class);
+        $categories = $repository->findAll();
+        $enAvant = $repository->findBy(
+            array('enAvant' => '1')
+        );
+
+        $repository = $entitymanager->getRepository(Prestataire::class);
+        $prestataires = $repository->findLatest();
+        
+        return $this->render('services/index.html.twig', [
+            "categories" => $categories,
+            "prestataires" => $prestataires,
+            "categorieEnAvant" => $enAvant[0],
+            "service" => $service
+        ]);
+    }
+}
